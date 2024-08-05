f72cb50210ad3a05b37a6b1ab232f692ba0616d7
Luan C. Redmann <luandacostaredmann@gmail.com>
2024-08-05 17:52:50 -0300

    :sparkles: inicia projeto Java no NetBeans

---
+++ ListaDeChamadaWebService/src/listadechamadawebservice/ListaDeChamadaWebService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listadechamadawebservice;
+
+/**
+ *
+ * @author 20182014040015
+ */
+public class ListaDeChamadaWebService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
